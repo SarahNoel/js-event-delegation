a4eced040aedb8549c95a0c0d3c7e49cb137a14c
Sarah Turek <snturek@gmail.com>
2015-07-29 16:09:14 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).on('ready', function() {
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    var toDo = $('input').val();
+    $('#all-todos').prepend('<li><h2>'+toDo+
+      '&nbsp;<button class="btn btn-danger btn-sm">X</button></h2></li>');
+  });
+
+  $(document).on("click", '.btn-danger', function(){
+    $(this).parent().parent().remove();
+  }
+    );
+
+
+
+});
